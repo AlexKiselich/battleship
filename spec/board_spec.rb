bbfd63cbb0ea5d3a4d4be26bbd8654b821dda1b0
Justin Pasko <jpasko1112@gmail.com>
2023-04-07 21:54:43 -0400

    Add a test to validate coordinates.

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -14,8 +14,20 @@ RSpec.describe Board do
   describe '#cells' do
     it 'has cells' do
       @board = Board.new
-require 'pry'; binding.pry
       expect(@board.cells).to be_a(Hash)
     end
   end
+
+  describe '#valid_coordinate?' do
+    it 'validates coordinates' do
+      @board = Board.new
+      expect(@board.valid_coordinate?('A1')).to eq(true)
+      expect(@board.valid_coordinate?('D4')).to eq(true)
+      expect(@board.valid_coordinate?('A5')).to eq(false)
+      expect(@board.valid_coordinate?('E1')).to eq(false)
+      expect(@board.valid_coordinate?('A22')).to eq(false)
+
+    end
+  end
+
 end   
