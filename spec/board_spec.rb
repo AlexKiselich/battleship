c967bafe71dc5b3987caf2244b0f7d7132b35fd6
Alex Kiselich <alexander.kiselich@gmail.com>
2023-04-08 19:45:48 -0600

    Add helper methods tests and the render method and test fot Board class

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -10,7 +10,6 @@ RSpec.describe Board do
   describe '#initialize' do
     it 'can initialize' do
       expect(@board).to be_a(Board)
-
     end
   end
 
@@ -46,8 +45,7 @@ RSpec.describe Board do
       expect(@board.valid_placement?(@cruiser, ["B1", "C1", "D1"])).to eq(true)
     end
 
-    it 'Places ships' do
-    
+    it '#place' do
       @board.place(@cruiser, ["A1", "A2", "A3"])
       cell_1 = @board.cells["A1"]
       cell_2 = @board.cells["A2"]
@@ -58,18 +56,68 @@ RSpec.describe Board do
       expect(cell_3.ship).to eq(@cruiser)
 
       expect(cell_3.ship == cell_2.ship).to eq(true)
-      
     end
 
     it 'prevents overlapping ships' do
       @board.place(@cruiser, ["A1", "A2", "A3"])
       expect(@board.valid_placement?(@submarine, ["A1", "B1"])).to eq(false)
-
-
     end
   end
+
+  describe "#render" do 
+    it 'can render' do
+      @board.place(@cruiser, ["A1", "A2", "A4"])
+      expect(@board.render).to eq("  1 2 3 4 \nA.... \nB.... \nC.... \nD.... \n")
+      expect(@board.render(true)).to eq("  1 2 3 4 \nA S S S . \nB . . . . \nC . . . . \nD . . . . \n")
+    end
+  end
+
+  #......Helper Methods tests.......
+
+  describe '#consecutive?' do
+    it 'can check if coordinates are consecutive' do
+      expect(@board.consecutive?(["A1", "A4"])).to eq(false)
+      expect(@board.consecutive?(["A1", "A2"])).to eq(true)
+    end
+  end
+
+  describe '#same_letter?' do
+    it 'can check it the coordinates have the same letter' do
+      expect(@board.same_letter?(["A1", "A2"])).to eq(true)
+      expect(@board.same_letter?(["A1", "B1"])).to eq(false)
+    end
+  end
+
+  describe '#same_number?' do
+    it 'can check it the coordinates have the same number' do
+      expect(@board.same_number?(["A1", "A2"])).to eq(false)
+      expect(@board.same_number?(["A1", "B1"])).to eq(true)
+    end
+  end
+
+  describe '#vertical?' do
+    it 'can check to see if coordinates are vertical' do
+      expect(@board.vertical?(["A1", "A2", "A3"])).to eq(false)
+      expect(@board.vertical?(["A1", "B1", "C1"])).to eq(true)
+    end
+  end
+
+  describe '#horizontal?' do
+    it 'can check to see if coordinates are horizontal' do
+      expect(@board.horizontal?(["A1", "A2", "A3"])).to eq(true)
+      expect(@board.horizontal?(["A1", "B1", "C1"])).to eq(false)
+    end
+  end
+
+  describe '#overlapping?' do
+    it 'can check to see if coordinates are overlapping' do
+      
+      expect(@board.overlapping?(["A1", "A2"])).to eq(true)
+      
+    end
+  end
+
 end
 
 
 
-     
