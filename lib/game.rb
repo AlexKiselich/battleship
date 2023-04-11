435cb2798de0fc41b791a943dbe9ca7ada9d2b67
Justin Pasko <jpasko1112@gmail.com>
2023-04-10 21:24:50 -0400

    Add several attributes to our initialize method and fix some spacing.

--- lib/game.rb
+++ lib/game.rb
@@ -1,9 +1,15 @@
 class Game
   attr_reader :board
+
   def initialize
-    @board = Board.new
-    @cruiser = Ship.new("Cruiser", 3)
-    @submarine = Ship.new("Submarine", 2)
+    @computer_player = Player.new
+    @player = Player.new
+    @computer_board = Board.new
+    @player_board = Board.new
+    @computer_cruiser = Ship.new("Cruiser", 3)
+    @computer_submarine = Ship.new("Submarine", 2)
+    @player_cruiser = Ship.new("Cruiser", 3)
+    @player_submarine = Ship.new("Submarine", 2)
   end
 
   def menu
@@ -11,22 +17,22 @@ class Game
     puts "What is your name?"
     name = gets.chomp
     puts "Hello #{name}, Welcome to Battleship"
-    puts "Please enter p to play a game or q to quit"
+    puts "Please enter 'p' to play a game or 'q' to quit"
     input = gets.chomp
       if input == "p" 
-        puts "play game"
+        puts "Play game!"
           elsif input == "q"
-          puts "See you next time"
+          puts "See you next time!"
         else
-         puts "invalid, please try again"
+         puts "Invalid, please try again!"
           menu
         end
   end
-  
+  # We want to create loops for both ships with keys.sample and 
+  # compare that to our method 'proper placement'.
   def computer_ship_placement
-    new_cords = []
-
-    new_cords << @board.cells.keys.sample(3)
+    new_coords = []
+    new_coords << @board.cells.keys.sample(3)
     board.place(@cruiser, ["A1", "A2", "A3"])
     # board.place(@submarine, ["B1", "B2"])
     puts "I have laid out my ships on the grid.
