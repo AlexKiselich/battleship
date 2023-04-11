4a0cb96a79bf92a14c184175054b88bf80128dd2
Justin Pasko <jpasko1112@gmail.com>
2023-04-11 18:05:55 -0400

    Added code to assist testing of turn method.

--- runner.rb
+++ runner.rb
@@ -2,6 +2,7 @@ require './lib/ship'
 require './lib/cell'
 require './lib/board'
 require './lib/game'
+require './lib/turn'
 
 
 game = Game.new
@@ -13,7 +14,8 @@ puts game.menu
 game.comp_ship_placement
 game.player_cruiser_placement
 game.player_submarine_placement
-puts game.comp_board.render
+# puts game.comp_board.render
 
 puts game.comp_board.render(true)
-puts game.player_board.render(true)
+# puts game.player_board.render(true)
+game.turn
